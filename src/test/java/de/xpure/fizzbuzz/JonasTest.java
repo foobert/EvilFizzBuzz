25b58fa93bbb781a3c4138f39553a3254134ff02
Jonas Herrig <j.herrig@xpure.de>
2019-01-22 15:05:29 +0100

    added test dummy, added .idea to gitignore

---
+++ src/test/java/de/xpure/fizzbuzz/JonasTest.java
@@ -0,0 +1,14 @@
+package de.xpure.fizzbuzz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RobertTests {
+
+    @Test
+    public void testFizzBuzz() {
+        Jonas jonasForTest = new JonasImpl();
+        //TODO WIP
+        Assertions.assertTrue(true);
+    }
+}
